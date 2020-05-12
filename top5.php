449d684b33098d42495878deddcd08b943bab226
Saish Mayekar <saishmayekar51@gmail.com>
2020-05-13 00:58:06 +0530

    c4

---
+++ top5.php
@@ -0,0 +1,190 @@
+<?php
+include './includes/header.php';
+ ?>
+
+
+
+  <div class="container mt-4">
+      <h1 class="display-4">TOP FIVE PLAYERS</h1>
+      <div class="row">
+        <div class="col-md-8">
+
+    <form action="" method="POST">
+
+          <div class="form-group">
+
+              <select class="form-control" name="p1" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+
+            <input name="p1p" type="text" class="form-control" placeholder="player 1 percentage">
+          </div>
+          <div class="form-group">
+
+              <select class="form-control" name="p2" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+
+            <input name="p2p" type="text" class="form-control" placeholder="player 2 percentage">
+          </div>
+          <div class="form-group">
+
+              <select class="form-control" name="p3" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+
+            <input name="p3p" type="text" class="form-control" placeholder="player 3 percentage">
+          </div>
+          <div class="form-group">
+
+              <select class="form-control" name="p4" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+
+            <input name="p4p" type="text" class="form-control" placeholder="player 4 percentage">
+          </div>
+          <div class="form-group">
+
+              <select class="form-control" name="p5" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+
+            <input name="p5p" type="text" class="form-control" placeholder="player 5 percentage">
+          </div>
+          <div class="form-group">
+            <input type="submit" value="SUBMIT" class="btn btn-dark btn-md"></div>
+    </form>
+  </div>
+</div>
+</div>
+
+
+
+
+<h1 class="display-4 mt-4">
+  Next match
+</h1>
+<form class="" action="" method="post">
+
+  <div class="container m-4">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-2">
+            <div class="form-group">
+              <select class="form-control" name="p1" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="form-group">
+              <select class="form-control" name="p2" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="form-group">
+              <select class="form-control" name="p3" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="form-group">
+              <select class="form-control" name="p4" id="">
+                <option value="Akshay D">Akshay D</option>
+                <option value="Akshay M">Akshay M</option>
+                <option value="Ashish">Ashish</option>
+                <option value="Mohit">Mohit</option>
+                <option value="Nikhil">Nikhil</option>
+                <option value="Rushabh">Rushabh</option>
+                <option value="Saish">Saish</option>
+                <option value="Vinayak">Vinayak</option>
+                <option value="YUvraj">YUvraj</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <input type="submit" class='btn btn-success' value="Add match">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+
+<?php
+
+include './includes/footer.php';
+ ?>
